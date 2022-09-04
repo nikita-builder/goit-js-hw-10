eb9eab1eced53e6ed121ddf5ba59a0594e426e76
nikita-builder <99048340+nikita-builder@users.noreply.github.com>
2022-09-04 21:49:19 +0300

    Initialize gh-pages

---
+++ 01-color-switcher.221935be.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");t.addEventListener("click",(function(e){n=setInterval((()=>{const e=`#${Math.floor(16777215*Math.random()).toString(16)}`;document.body.style.background=e,t.setAttribute("disabled",!0)}),1e3)})),e.addEventListener("click",(function(){t.removeAttribute("disabled"),clearInterval(n)}));let n=null;
+//# sourceMappingURL=01-color-switcher.221935be.js.map
---
+++ 01-color-switcher.8a60e420.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");t.addEventListener("click",(function(e){n=setInterval((function(){var e="#".concat(Math.floor(16777215*Math.random()).toString(16));document.body.style.background=e,t.setAttribute("disabled",!0)}),1e3)})),e.addEventListener("click",(function(){t.removeAttribute("disabled"),clearInterval(n)}));var n=null}();
+//# sourceMappingURL=01-color-switcher.8a60e420.js.map
